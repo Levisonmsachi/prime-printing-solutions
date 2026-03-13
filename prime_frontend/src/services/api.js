b965add32906e872efd47c1ec9935829e683fff4
Levisonmsachi <levisonmsachi03@gmail.com>
2026-03-13 10:14:07 +0200

    Update API base URL for production backend

--- prime_frontend/src/services/api.js
+++ prime_frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+// for localhost
+//const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "https://prime-printing-solutions.onrender.com/api";
 
 // Get token from localStorage
 const getToken = () => localStorage.getItem("admin_token");
