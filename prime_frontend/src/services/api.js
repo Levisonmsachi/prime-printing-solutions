275812745d2e49064f661515c2ebe9a4cf9ef34b
LEVVIE-LIVVIE <129958236+Levisonmsachi@users.noreply.github.com>
2026-03-13 11:20:39 +0200

    Update api.js

--- prime_frontend/src/services/api.js
+++ prime_frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // for localhost
 //const API_BASE_URL = "http://127.0.0.1:8000/api";
-const API_BASE_URL = import.meta.env.VITE_API_URL + '/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
 
 // Get token from localStorage
 const getToken = () => localStorage.getItem("admin_token");
